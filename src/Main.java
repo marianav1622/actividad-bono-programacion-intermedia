5d8f41a0cd693ac38a7f68357055c8ede4e8e102
Mariana <mvreyesp@libertadores.edu.co>
2026-05-03 03:53:23 +0000

    mostrar

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         // 1. Declaración de variables
         int a = 10;
         Persona p = new Persona("Carlos");
+         // Mostrar valores iniciales
+        System.out.println("--- ANTES de llamar al método ---");
+        System.out.println("Valor de a (primitivo): " + a);
+        System.out.println("Nombre de p (objeto): " + p.nombre);
+        System.out.println();
+
     }
 }
